3f4ff08e4aaf26e0755072d96447188fe4382f5d
Zack Brown <zack.brown@3squared.com>
2018-11-16 12:03:13 +0000

    Update project settings to reset code signing

--- PeakResult/Core/PeakResult.h
+++ PeakResult/Core/PeakResult.h
@@ -23,5 +23,3 @@ FOUNDATION_EXPORT double PeakResultVersionNumber;
 FOUNDATION_EXPORT const unsigned char PeakResultVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PeakResult/PublicHeader.h>
-
-
